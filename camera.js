7c76c5b01c5ca837d5fa725ada3995ece9e0fe76
Stepan Gharslyan <stepan.gharslyan@gmail.com>
2013-11-24 17:08:07 +0400

    changed again

--- camera.js
+++ camera.js
@@ -1,30 +1,14 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 var onDeviceReady = function() {
-	var pictureSource = navigator.camera.PictureSourceType;
- 	var destinationType = navigator.camera.DestinationType;
- 	 var onPhotoDataSuccess = function(imageURI) {
-      
-      var smallImage = document.getElementById('smallImage');
-      smallImage.style.display = 'block';
-      smallImage.src = imageURI;
-    };
-    var capturePhoto = function() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL });
-    };
+ 	navigator.camera.getPicture( onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+	});
+ 		var onSuccess = function(imageData) {
+    	var image = document.getElementById('myImage');
+    		image.src = "data:image/jpeg;base64," + imageData;
+		};
 
-    var capturePhotoEdit = function() {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-        destinationType: destinationType.DATA_URL });
-    };
-
-    var getPhoto = function(source) {
-      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.FILE_URI,
-        sourceType: source });
-    };
-
-    var onFail = function(message) {
-      alert('Չստացվեց, որովհետև: ' + message);
-    }
+		var onFail = function(message) {
+    	alert('Failed because: ' + message);
+		}
 };
